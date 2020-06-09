5bc611d2e4a7af2a834cc3613cc295e86c644b39
JoDDeok <joddeok@sju.ac.kr>
2020-06-09 23:20:18 +0900

    bgm&survey

--- routes/posts.js
+++ routes/posts.js
@@ -54,53 +54,6 @@ router.post('/', util.isLoggedin, function(req, res){
   });
 });
 
-// show
-router.get('/:id', function(req, res){
-  Post.findOne({_id:req.params.id})
-    .populate('author')
-    .exec(function(err, post){
-      if(err) return res.json(err);
-      res.render('posts/show', {post:post});
-    });
-});
-
-// edit
-router.get('/:id/edit', util.isLoggedin, checkPermission, function(req, res){
-  var post = req.flash('post')[0];
-  var errors = req.flash('errors')[0] || {};
-  if(!post){
-    Post.findOne({_id:req.params.id}, function(err, post){
-        if(err) return res.json(err);
-        res.render('posts/edit', { post:post, errors:errors });
-      });
-  }
-  else {
-    post._id = req.params.id;
-    res.render('posts/edit', { post:post, errors:errors });
-  }
-});
-
-// update
-router.put('/:id', util.isLoggedin, checkPermission, function(req, res){
-  req.body.updatedAt = Date.now();
-  Post.findOneAndUpdate({_id:req.params.id}, req.body, {runValidators:true}, function(err, post){
-    if(err){
-      req.flash('post', req.body);
-      req.flash('errors', util.parseError(err));
-      return res.redirect('/posts/'+req.params.id+'/edit');
-    }
-    res.redirect('/posts/'+req.params.id);
-  });
-});
-
-// destroy
-router.delete('/:id', util.isLoggedin, checkPermission, function(req, res){
-  Post.deleteOne({_id:req.params.id}, function(err){
-    if(err) return res.json(err);
-    res.redirect('/posts');
-  });
-});
-
 module.exports = router;
 
 // private functions
--- routes/surveys.js
+++ routes/surveys.js
@@ -3,16 +3,34 @@ var router = express.Router();
 var Game = require('../models/Game');
 var util = require('../util');
 
-// Surveys
+// Survey - 1
 router.get('/', function(req, res){
-  res.render('surveys/survey');
+  res.render('surveys/survey-1');
+});
+
+// Survey - 2
+router.get('/survey-2', function(req, res){
+  var type = req.query.type;
+
+  res.render('surveys/survey-2', {
+    type:type
+  });
+});
+
+// Survey - 3
+router.get('/survey-3', function(req, res){
+  var type = req.query.type;
+
+  res.render('surveys/survey-3', {
+    type:type
+  });
 });
 
 // Survey Result
 router.get('/result', async function(req, res){
   var game = await Game.findOne({name:req.query.resultGame})
     .exec();
-    
+
   res.render('surveys/survey-result', {
     game:game
   });
